481d4dcc95c3912f3aa6f9b6af87cc6d1943f696
Darshan Mohan Tak <darshan.mohan@juspay.in>
2024-07-07 12:53:52 +0530

    understanding go concurrency

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
-"fmt"
+	"fmt"
+	"time"
 )
 
+func routine() {
+	count := 0
+	for i := 0; i < 1000; i++ {
+		count += 1
+	}
+
+	fmt.Println("This is the first go routine")
+}
 
 func main() {
-fmt.Println("Hi, this is the playground for Go")
+	go routine()
+	fmt.Println("Hi, this is the playground for Go")
+	time.Sleep(1000 * time.Millisecond)
 }

d6ef5f636904822649c55dd882bdb2b3caacc4d8
Darshan Mohan Tak <darshan.mohan@juspay.in>
2024-12-17 20:09:36 +0530

    initial automation

--- main.go
+++ main.go
@@ -1,10 +1,30 @@
 package main
 
 import (
-"fmt"
+	"fastline/exercises"
+	"fmt"
+	"os"
 )
 
+var exerciseMapper = map[string]func(){
+	"exercise1": exercises.Exercise1,
+}
 
 func main() {
-fmt.Println("Hi, this is the playground for Go")
+	if len(os.Args) < 2 {
+		fmt.Println("Usage : go run main.go <exerciseID>")
+		fmt.Println("Available exercises are :")
+		for k := range exerciseMapper {
+			fmt.Println(k)
+		}
+		return
+	}
+
+	exerciseId := os.Args[1]
+	if exercise, exists := exerciseMapper[exerciseId] ; exists{
+		fmt.Printf("Running %s....",exerciseId)
+		exercise()
+	} else{
+		fmt.Println("Exercise not found")
+	}
 }
